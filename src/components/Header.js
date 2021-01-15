a05842f6ae1f43db0841c8cbcebae8cb8443f184
Amos Rose <amosroseacct@gmail.com>
2021-01-15 16:52:54 -0700

    logo and favicon added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <div className="header-wrapper">
+      <img src={logo} alt="Shoppies logo" />
       <h1>The Shoppies</h1>
     </div>
   );
